25a3330e719b5cee18ff0b1ed0dc91613979c9ce
valentino <v.thorey@gmail.com>
2017-03-13 23:39:26 +0100

    Randomopt ready.

--- benderopt/base.py
+++ benderopt/base.py
@@ -8,3 +8,11 @@ class BaseOptimizer:
 
     def suggest(self, data):
         raise NotImplementedError
+
+    def is_sample_unique(self, sample):
+        is_unique = True
+        if not hasattr(self, '_trials_parameters'):
+            self._trials_parameters = [trial["parameters"] for trial in self.data["trials"]]
+        if sample in self._trials_parameters:
+            is_unique = False
+        return is_unique
