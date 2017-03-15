cc2b02590aae7497637fedabb6c4ca14a043dac2
Valentino <v.thorey@gmail.com>
2017-03-15 20:50:34 +0100

    Vectorized sample drawing and density point evaluation for speedup (x1000)

--- benderopt/minimizer.py
+++ benderopt/minimizer.py
@@ -1,21 +1,24 @@
 from benderopt.optimizer import optimizers
 from benderopt.base import OptimizationProblem, Observation
 import numpy as np
+import time
 
 
 def minimize(f,
              optimization_problem,
-             optimizer_type="random",
+             optimizer_type="tpe",
              number_of_evaluation=100):
     if type(optimization_problem) == list:
         optimization_problem = OptimizationProblem.from_list(optimization_problem)
 
     for _ in range(number_of_evaluation):
+        t1 = time.time()
         optimizer = optimizers[optimizer_type](optimization_problem)
         sample = optimizer.suggest()
         loss = f(**sample)
         observation = Observation.from_dict({"loss": loss, "sample": sample})
         optimization_problem.add_observation(observation)
+        print(time.time() - t1)
     return optimization_problem.best_sample
 
 
@@ -35,4 +38,4 @@ if __name__ == "__main__":
         }
     ]
 
-    best_sample = minimize(f, optimization_problem, number_of_evaluation=5000)
+    best_sample = minimize(f, optimization_problem, number_of_evaluation=1000)
--- benderopt/utils_stats.py
+++ benderopt/utils_stats.py
@@ -3,97 +3,147 @@ from numpy import random
 from scipy import stats
 
 
-def generate_sample_uniform(min, max, log=False, step=None):
-    sample = random.uniform(low=min,
-                            high=max)
+def generate_samples_uniform(min, max, log=False, step=None, size=1):
+    samples = random.uniform(low=min,
+                             high=max,
+                             size=size)
     if log:
-        sample = np.exp(sample)
+        samples = np.exp(samples)
     if step:
-        sample = step * round(sample / step)
-    return sample
+        samples = step * round(samples / step)
+    return samples
 
 
-def generate_sample_normal(mu, sigma, min=None, max=None, log=False, step=None, max_retry=50):
-    # Draw a sample which fit between min and max (if they are given)
+def generate_samples_normal(mu,
+                            sigma,
+                            min=None,
+                            max=None,
+                            log=False,
+                            step=None,
+                            size=1,
+                            max_retry=50):
+    # Draw a samples which fit between min and max (if they are given)
+    samples = np.ones(size) * np.nan
+    nans_locations = np.where(np.isnan(samples))[0]
+    min = min if min else -np.inf
+    max = max if max else np.inf
     for _ in range(max_retry):
-        sample = random.normal(loc=mu, scale=sigma)
-        if min and sample < min:
-            continue
-        if max and sample > max:
-            continue
-        break
+        samples[nans_locations] = random.normal(loc=mu, scale=sigma, size=len(nans_locations))
+        samples[(samples < min) * (samples > max)] = np.nan
+        nans_locations = np.where(np.isnan(samples))[0]
+        if len(nans_locations) == 0:
+            break
     else:
         raise ValueError("No sample could be drawn in given bounds with max_retry {}".format(
             max_retry))
 
     if log:
-        sample = np.exp(sample)
+        samples = np.exp(samples)
+
     if step:
-        sample = step * round(sample / step)
-    return sample
+        samples = step * round(samples / step)
+
+    return samples
 
 
-def generate_sample_categorical(values, weights):
-    return random.choice(values, p=weights)
+def generate_samples_categorical(values, weights, size=1):
+    return random.choice(values, p=weights, size=size)
 
 
-def categorical_logpdf(sample, values, weights):
-    return np.log(weights[values.find(sample)])
-
-
-def generate_sample_gaussian_mixture(mus,
-                                     sigmas,
-                                     weights=None,
-                                     min=None,
-                                     max=None,
-                                     log=False,
-                                     step=None,
-                                     max_retry=50):
+def generate_samples_gaussian_mixture(mus,
+                                      sigmas,
+                                      weights=None,
+                                      min=None,
+                                      max=None,
+                                      log=False,
+                                      step=None,
+                                      size=1,
+                                      max_retry=50):
     """ Generate a random sample according to a mixture of gaussians."""
 
     number_of_gaussian = len(mus)
-    selected_gaussian = random.choice(range(number_of_gaussian), p=weights)
+    selected_gaussian = random.choice(range(number_of_gaussian), p=weights, size=size)
+    return np.concatenate([
+        generate_samples_normal(mu=mu,
+                                sigma=sigma,
+                                min=min,
+                                max=max,
+                                step=step,
+                                log=log,
+                                size=np.sum(selected_gaussian == i),
+                                max_retry=max_retry)
+        for i, (mu, sigma) in enumerate(zip(mus, sigmas))
+    ])
 
-    return generate_sample_normal(mu=mus[selected_gaussian],
-                                  sigma=sigmas[selected_gaussian],
-                                  min=min,
-                                  max=max,
-                                  step=step,
-                                  log=log,
-                                  max_retry=max_retry)
+
+sample_generators = {
+    "uniform": generate_samples_uniform,
+    "normal": generate_samples_normal,
+    "categorical": generate_samples_categorical,
+    "gaussian_mixture": generate_samples_gaussian_mixture,
+}
 
 
-def normal_pdf(sample,
+def categorical_logpdf(samples, values, weights):
+    converter = dict(zip(values, weights))
+    return np.log([converter[value] for value in values])
+
+
+def normal_cdf(samples,
                mu,
                sigma,
                min=None,
                max=None,
                log=False):
+    """Redefine (log)normal cdf taking min and max into account."""
     distribution = stats.norm if not log else stats.lognorm
 
-    value = distribution.pdf(sample, loc=mu, scale=sigma)
-    # rescale if needed
-    value /= (distribution.cdf(max if max else np.inf, loc=mu, scale=sigma) -
-              distribution.cdf(min if min else -np.inf, loc=mu, scale=sigma))
-    return value
+    values = distribution.cdf(np.clip(samples, a_min=None, a_max=max), loc=mu, scale=sigma)
+
+    if min:
+        values -= distribution.cdf(min, loc=mu, scale=sigma)
+        values = np.clip(values, a_min=0, a_max=None)
+
+    values /= (distribution.cdf(max if max else np.inf, loc=mu, scale=sigma) -
+               distribution.cdf(min if min else -np.inf, loc=mu, scale=sigma))
+
+    return values
 
 
-def gaussian_mixture_logpdf(sample,
+def normal_pdf(samples,
+               mu,
+               sigma,
+               min=None,
+               max=None,
+               log=False,
+               step=None):
+    """Redefine (log)normal pdf taking min and max into account."""
+    values = None
+    if step is None:
+        distribution = stats.norm if not log else stats.lognorm
+
+        values = distribution.pdf(samples, loc=mu, scale=sigma)
+        # rescale if needed
+        values /= (distribution.cdf(max if max else np.inf, loc=mu, scale=sigma) -
+                   distribution.cdf(min if min else -np.inf, loc=mu, scale=sigma))
+        values[samples < (min if min else -np.inf)] = 0
+        values[samples > (max if max else np.inf)] = 0
+    else:
+        values = (normal_cdf(samples + step / 2, mu=mu, sigma=sigma, min=min, max=max, log=log) -
+                  normal_cdf(samples - step / 2, mu=mu, sigma=sigma, min=min, max=max, log=log))
+    return values
+
+
+def gaussian_mixture_logpdf(samples,
                             mus,
                             sigmas,
                             weights=None,
                             min=None,
                             max=None,
-                            log=False):
+                            log=False,
+                            step=None):
     if weights is None:
         weights = np.ones(len(mus)) / len(mus)
-    return np.log(np.sum([normal_pdf(mu=mu, sigma=sigma, min=min, max=max, log=log) * weights
-                          for mu, sigma, weight in zip(mus, sigmas, weights)]))
-
-
-sample_generators = {
-    "uniform": generate_sample_uniform,
-    "normal": generate_sample_normal,
-    "categorical": generate_sample_categorical,
-    "gaussian_mixture": generate_sample_gaussian_mixture,
-}
+    return np.log(np.clip(np.sum(
+        [normal_pdf(samples, mu=mu, sigma=sigma, min=min, max=max, log=log, step=step) * weight
+         for mu, sigma, weight in zip(mus, sigmas, weights)], axis=0), a_min=1e-20, a_max=None))
