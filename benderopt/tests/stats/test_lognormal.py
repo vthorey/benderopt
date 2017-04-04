ab8a01c59be48911ad2e19ca922ae591290bfc60
valentino <v.thorey@gmail.com>
2017-04-04 23:42:16 +0200

    start lognormal.

--- benderopt/tests/stats/test_lognormal.py
+++ benderopt/tests/stats/test_lognormal.py
@@ -1,5 +1,119 @@
 from benderopt.stats import sample_generators
 from benderopt.stats import probability_density_function
+from benderopt.utils import logb
 import numpy as np
 
 np.random.seed(0)
+
+
+# def test_lognormal_generator():
+#     """Test to reassure."""
+#     mu = 1e-5
+#     sigma = 1e1
+#     low = 1e-7
+#     high = 1e1
+#     base = 10
+#     step = None
+#     size = 100000
+#     epsilon = 1e-1
+
+#     samples = sample_generators["lognormal"](size=size,
+#                                              mu=mu,
+#                                              sigma=sigma,
+#                                              low=low,
+#                                              high=high,
+#                                              base=base,
+#                                              step=step)
+#     # Median
+#     theorical_median = np.exp(logb(mu, base))
+#     assert np.abs(np.median(samples) - theorical_median) / theorical_median < epsilon
+#     # mean (expected value)
+#     theorical_mean = np.exp(logb(mu, base) + (logb(sigma, base) ** 2) / 2)
+#     assert np.abs(np.mean(samples) - theorical_mean) / theorical_mean < epsilon
+#     assert np.sum(samples < low) == 0
+#     assert np.sum(samples >= high) == 0
+
+
+# def test_lognormal_generator_step():
+#     """Test to reassure."""
+#     mu = 50
+#     sigma = 3
+#     low = 0
+#     high = 100
+#     base = 10
+
+#     step = 2
+#     size = 100000
+
+#     samples = sample_generators["lognormal"](size=size,
+#                                              mu=mu,
+#                                              sigma=sigma,
+#                                              low=low,
+#                                              high=high,
+#                                              base=base,
+#                                              step=step)
+#     assert np.sum(samples % 2) == 0
+#     assert np.sum(samples < low) == 0
+#     assert np.sum(samples >= high) == 0
+
+
+# def test_lognormal_pdf():
+#     mu = 50
+#     sigma = 20
+#     low = 0
+#     high = 100
+#     base = 10
+
+#     step = None
+#     size = 100000
+#     bins = 10000
+#     epsilon = 1e-1
+#     samples = sample_generators["lognormal"](size=size,
+#                                              low=low,
+#                                              high=high,
+#                                              base=base,
+#                                              step=step,
+#                                              mu=mu,
+#                                              sigma=sigma)
+#     hist, bin_edges = np.histogram(samples, bins=bins, normed=True)
+#     densities = probability_density_function["lognormal"](
+#         samples=(bin_edges[1:] + bin_edges[:-1]) * 0.5,
+#         low=low, high=high,
+#         base=base, step=step, mu=mu, sigma=sigma)
+#     assert np.sum(densities[samples < low]) == 0
+#     assert np.sum(densities[samples >= high]) == 0
+#     assert ((hist - densities) / densities).mean() <= epsilon
+
+
+# def test_lognormal_pdf_step():
+#     mu = 50
+#     sigma = 20
+#     low = 0
+#     high = 100
+#     base = 10
+
+#     step = 2
+#     size = 100000
+#     epsilon = 1e-1
+
+#     samples = sample_generators["lognormal"](size=size,
+#                                              low=low,
+#                                              high=high,
+#                                              base=base,
+#                                              step=step,
+#                                              mu=mu,
+#                                              sigma=sigma)
+
+#     hist, bin_edges = np.histogram(samples,
+#                                    bins=np.arange(low - step / 10, high, step),
+#                                    normed=True)
+#     densities = probability_density_function["lognormal"](
+#         samples=bin_edges[0:-1] + step / 10,
+#         low=low,
+#         high=high,
+#         base=base,
+#         step=step,
+#         mu=mu, sigma=sigma)
+#     assert np.sum(densities[samples < low]) == 0
+#     assert np.sum(densities[samples >= high]) == 0
+#     assert ((hist - densities) / densities).mean() <= epsilon
