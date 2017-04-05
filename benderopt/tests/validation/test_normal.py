dbaa8c3fbfc37870e4ff7a955a4928ef2134f212
Valentino <v.thorey@gmail.com>
2017-04-05 18:59:54 +0200

    Test for normal distribution.

---
+++ benderopt/tests/validation/test_normal.py
@@ -0,0 +1,139 @@
+import pytest
+from benderopt.validation.normal import validate_normal, validate_normal_value
+import numpy as np
+
+
+def test_normal_search_space_ok():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": -5,
+        "high": 5,
+        "step": 0.1,
+    }
+
+    search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_not_dict():
+    search_space = [0.5, 1],
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_no_mu():
+    search_space = {
+        "sigma": 1,
+        "low": -5,
+        "high": 5,
+        "step": 0.1,
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_no_sigma():
+    search_space = {
+        "mu": 0.5,
+        "low": -5,
+        "high": 5,
+        "step": 0.1,
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_bad_low():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": [-5],
+        "high": 5,
+        "step": 0.1,
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_bad_high():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": -5,
+        "high": [5],
+        "step": 0.1,
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_bad_low_high():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": 5,
+        "high": 5,
+        "step": 0.1,
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_set_low_high():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "step": 0.1,
+    }
+
+    search_space = validate_normal(search_space)
+
+    assert "low" in search_space.keys()
+    assert search_space["low"] == -np.inf
+    assert "high" in search_space.keys()
+    assert search_space["high"] == np.inf
+
+
+def test_normal_search_space_step():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": -5,
+        "high": 5,
+        "step": {0.1},
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_step_high():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": -5,
+        "high": 5,
+        "step": 5,
+    }
+
+    with pytest.raises(ValueError):
+        search_space = validate_normal(search_space)
+
+
+def test_normal_search_space_set_step():
+    search_space = {
+        "mu": 0.5,
+        "sigma": 1,
+        "low": -5,
+        "high": 5,
+    }
+
+    search_space = validate_normal(search_space)
+    assert "step" in search_space.keys()
+    assert search_space["step"] is None
