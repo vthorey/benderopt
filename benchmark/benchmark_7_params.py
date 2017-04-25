d4f900fe16e2467c32f8b8a2eb7e1a44429d29fc
Valentino <v.thorey@gmail.com>
2017-04-25 17:21:06 +0200

    Bencmarks.

--- benchmark/benchmark_7_params.py
+++ benchmark/benchmark_7_params.py
@@ -128,6 +128,46 @@ best_sample = {
 }
 
 
+np.random.seed(0)
+seeds = np.random.randint(low=0, high=2 ** 31 - 1, size=5)
+methods = ["parzen_estimator", "model_based_estimator", "random"]
+number_of_trials = [30, 50, 100, 200, 500]
+trials = {
+    method: {
+        n: []
+        for n in number_of_trials
+    }
+    for method in methods
+}
+for seed in seeds:
+    for n in number_of_trials:
+        for method in methods:
+            print("N: {}".format(n))
+            np.random.seed(seed)
+            best = minimize(function_to_optimize,
+                            optimization_problem,
+                            optimizer_type=method,
+                            number_of_evaluation=n)
+            trials[method][n].append(function_to_optimize(**best))
+        # print(trials)
+results = {
+    method: {
+        n: {
+            "mean": np.mean(values),
+            "std": np.std(values),
+        }
+        for n, values in ns.items()
+    }
+    for method, ns in trials.items()
+}
+
+for n in number_of_trials:
+    print("\nN:{}\n".format(n))
+    print("Random: {}/{}".format(results["random"][n]["mean"], results["random"][n]["std"]))
+    print("parzen: {}/{}".format(results["parzen_estimator"][n]["mean"], results["parzen_estimator"][n]["std"]))
+    print("model_: {}/{}".format(results["model_based_estimator"][n]["mean"], results["model_based_estimator"][n]["std"]))
+
+
 def test_random_uniform():
     np.random.seed(0)
     minimize(function_to_optimize,
