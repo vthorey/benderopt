1d47b1b18fab2a4fd3a81802fea09db97588c08e
Valentino <v.thorey@gmail.com>
2019-01-04 10:19:00 +0100

    [optimizer] add random parzen estimator

--- benderopt/optimizer/__init__.py
+++ benderopt/optimizer/__init__.py
@@ -1,5 +1,6 @@
 from .random import RandomOptimizer
 from .parzen_estimator import ParzenEstimator
+from .random_parzen_estimator import RandomParzenEstimator
 from .optimizer import BaseOptimizer
 from .model_based_estimator import ModelBasedEstimator
 
@@ -7,5 +8,6 @@ optimizers = {
     "base": BaseOptimizer,
     "random": RandomOptimizer,
     "parzen_estimator": ParzenEstimator,
+    "random_parzen_estimator": RandomParzenEstimator,
     "model_based_estimator": ModelBasedEstimator,
 }
