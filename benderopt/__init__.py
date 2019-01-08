08e4e8ec7f4c8494adbb952144e3990de9ba771a
Valentino <v.thorey@gmail.com>
2019-01-08 15:09:56 +0100

    1.3.1

--- benderopt/__init__.py
+++ benderopt/__init__.py
@@ -4,4 +4,4 @@ __all__ = [
     "minimize"
 ]
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # encoding: utf-8
-from setuptools import setup, find_packages
+from setuptools import setup
 
 import benderopt
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
 
@@ -26,6 +29,10 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
     ],
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
 
     install_requires=[
         'numpy>=1.15.4',
