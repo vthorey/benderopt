59b82d7e421832ec8c75c686fec77ae69e9b3212
valentin <v.thorey@gmail.com>
2020-10-22 15:50:57 +0200

    Improve packaging thanks to @tchar

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 # encoding: utf-8
 from setuptools import setup
 
-import benderopt
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -10,15 +8,19 @@ with open("README.md", "r") as fh:
 setup(
     name="benderopt",
     # la version du code
-    version=benderopt.__version__,
-    packages=["benderopt"],
+    version="1.3.2",
+    packages=['benderopt',
+              'benderopt.validation',
+              'benderopt.base',
+              'benderopt.optimizer',
+              'benderopt.stats'],
     author="Valentin Thorey",
     author_email="v.thorey@gmail.com",
     description="Black Box optimization library.",
     include_package_data=True,
     url="https://github.com/Dreem-Organization/benderopt",
-    classifiers=["Programming Language :: Python", "Programming Language :: Python :: 3",],
+    classifiers=["Programming Language :: Python", "Programming Language :: Python :: 3", ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["numpy>=1.15.4", "scipy>=1.1.0",],
+    install_requires=["numpy>=1.15.4", "scipy>=1.1.0", ],
 )
