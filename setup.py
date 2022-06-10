c240a61ef739b0abed3ee7ce20c27d7017bea816
Valentin Thorey <valentin.thorey@dataiku.com>
2022-06-10 08:46:11 +0200

    1.4.0

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="benderopt",
-    # la version du code
-    version="1.3.2",
+    version="1.4.0",
     packages=[
         "benderopt",
         "benderopt.validation",
